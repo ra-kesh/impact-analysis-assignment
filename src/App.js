168fbb34d30579c0eadbe6cb5b127cc93e1a1db9
Rakesh <iamrakeshkumar@pm.me>
2021-11-26 18:39:00 +0530

    feat:data fetched and loaded on the react table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useMemo, useState } from "react";
+import axios from "axios";
+import ReceipeTable from "./components/ReceipeTable";
 
 function App() {
+  const [receipes, setReceipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchReceipes = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios(
+        "https://s3-ap-southeast-1.amazonaws.com/he-public-data/reciped9d7b8c.json"
+      );
+
+      setReceipes(data);
+      setLoading(false);
+    } catch (error) {
+      setReceipes([]);
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchReceipes();
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+      },
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Category",
+        accessor: "category",
+      },
+      {
+        Header: "Label",
+        accessor: "label",
+      },
+      {
+        Header: "Price",
+        accessor: "price",
+      },
+      {
+        Header: "Description",
+        accessor: "description",
+      },
+    ],
+
+    []
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <h2>loading...</h2>
+      ) : (
+        <ReceipeTable columns={columns} data={receipes} />
+      )}
     </div>
   );
 }
